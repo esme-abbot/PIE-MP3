63ef7b3f9e4028f9a20afde06a43f54713d6540d
hnvakil <78379253+hnvakil@users.noreply.github.com>
2021-10-17 18:07:38 -0400

    Changes sliders to MOdes

--- button_control.py
+++ button_control.py
@@ -1,9 +1,10 @@
+
 import tkinter as tk
 import serial
 import time
 
 
-arduino = serial.Serial('COM8', 115200, timeout=.1)
+arduino = serial.Serial('COM6', 115200, timeout=.1)
 
 # def getData():
 #  	while True:
@@ -20,7 +21,7 @@ def send_to_arduino(message):
     else:
         arduino.write(message.encode())
         print("Sent Message: " + message)
- 	# print(getData())
+    # print(getData())
     
 def read_slider(event):
     val = speed_slider.get()
@@ -31,15 +32,25 @@ def read_slider(event):
 
 window = tk.Tk()
 
-slider_label = tk.Label(window, text = "Set the Center Speed")
-slider_label.pack()
-speed_slider = tk.Scale(window, from_= 0, to = 100, orient='horizontal', length = 300, command = read_slider)
-speed_slider.pack()
-speed_slider.set(50)
+# slider_label = tk.Label(window, text = "Set the Center Speed")
+# slider_label.pack()
+# speed_slider = tk.Scale(window, from_= 0, to = 100, orient='horizontal', length = 300, command = read_slider)
+# speed_slider.pack()
+# speed_slider.set(50)
 stop_button = tk.Button(window, text="Stop MO!", command = lambda: send_to_arduino("stop"))
 stop_button.pack()
 go_button = tk.Button(window, text="Go MO Go!", command = lambda: send_to_arduino("go"))
 go_button.pack()
+
+snail_mode = tk.Button(window, text="Snail Mode", command = lambda: send_to_arduino("snail"))
+snail_mode.pack()
+
+normal_mode = tk.Button(window, text="Normal Mode", command = lambda: send_to_arduino("normal"))
+normal_mode.pack()
+
+XTreme_mode = tk.Button(window, text="XTREME MODE !!1!!!", command = lambda: send_to_arduino("extreme"))
+XTreme_mode.pack()
+
 
 
 
